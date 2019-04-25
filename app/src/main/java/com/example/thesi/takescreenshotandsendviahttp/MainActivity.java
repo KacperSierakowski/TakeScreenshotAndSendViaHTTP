dbef1456c4553d323c35ba9933b19e56317264b2
Student218608 <218608@student.pwr.edu.pl>
2019-04-25 17:09:11 +0200

    BackgroundThread

--- app/src/main/java/com/example/thesi/takescreenshotandsendviahttp/MainActivity.java
+++ app/src/main/java/com/example/thesi/takescreenshotandsendviahttp/MainActivity.java
@@ -2,15 +2,14 @@ package com.example.thesi.takescreenshotandsendviahttp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -24,21 +23,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     TextView ResponseFromRequestTextView;
+    Button startThreadButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button btn = (Button) findViewById(R.id.ScreenshotButton);
-        ResponseFromRequestTextView=(TextView)findViewById(R.id.ResponseFromRequestTextView);
+        startThreadButton = (Button) findViewById(R.id.StartThreadButton);
+        ResponseFromRequestTextView = (TextView) findViewById(R.id.ResponseFromRequestTextView);
         //TakeScreen();
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -47,12 +47,19 @@ public class MainActivity extends AppCompatActivity {
                 takeScreenshot();
             }
         });
-
-        }
+        startThreadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExampleBackgroundThread thread = new ExampleBackgroundThread(10);
+                thread.start();
+                //SleepMainThread(10);
+            }
+        });
+    }
 
     private void TakeScreen() {
-        int i=1;
-        while(i<5){
+        int i = 1;
+        while (i < 5) {
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
                 @Override
@@ -64,30 +71,71 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void SleepMainThread(int seconds) {
+        for (int i = 0; i < seconds; i++) {
+            Log.d(TAG, "startThread: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class ExampleBackgroundThread extends Thread {
+        int seconds;
+
+        ExampleBackgroundThread(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < seconds; i++) {
+                Log.d(TAG, "startThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                    takeScreenshot();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     private void takeScreenshot() {
-        Date now = new Date();
-        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
         try {
-            String mScreenFilePath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
             View view = getWindow().getDecorView().getRootView();
             view.setDrawingCacheEnabled(true);
             Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
             view.setDrawingCacheEnabled(false);
-
+            File imageFile = SaveBitmapReturnFile(bitmap);
+            String url = "";
+            //sendScreenshot(url,mScreenFilePath);
+            if(imageFile!=null){
+               // openScreenshot(imageFile);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+    public File SaveBitmapReturnFile(Bitmap bitmap) {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+        try {
+            String mScreenFilePath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
             File imageFile = new File(mScreenFilePath);
             FileOutputStream outputStream = new FileOutputStream(imageFile);
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
             outputStream.flush();
             outputStream.close();
-
-            String url="";
-            sendScreenshot(url,mScreenFilePath);
-            //openScreenshot(imageFile);
+            return imageFile;
         } catch (Throwable e) {
             e.printStackTrace();
         }
+        return null;
     }
+
     public void sendScreenshot(String url, String imagePath){
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.cancelAll(new RequestQueue.RequestFilter() {
